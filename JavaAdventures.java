5f814ce0db5509ef8e9f38d6b4561305da8cfee5
DerKrull <ls2016@web.de>
2020-12-15 14:02:30 +0100

    Fixed part of the errors

--- JavaAdventures.java
+++ JavaAdventures.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class JavaAdventures {
   public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-   	String playerName;
-        int playerChoice;
-/*TEST..........................................................................................................................................*/
+    Scanner scanner = new Scanner(System.in);
+    String playerName;
+    int playerChoice;
+    /*TEST..........................................................................................................................................*/
     System.out.println("You stranded on an island.");
     System.out.println("What is your name?");
     String name = scanner.nextLine();
