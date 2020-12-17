9d6d03473390ed879fe3d94a40904522be7af984
DerKrull <ls2016@web.de>
2020-12-17 15:51:58 +0100

    Fixed Identation level

--- LiveChoice.java
+++ LiveChoice.java
@@ -1,26 +1,26 @@
 public class LiveChoice {
-    private String description;
-    private int nextStep;
+  private String description;
+  private int nextStep;
 
-    public LiveChoice(String description, int nextStep) {
-        this.description = description;
-        this.nextStep = nextStep;
-    }
+  public LiveChoice(String description, int nextStep) {
+    this.description = description;
+    this.nextStep = nextStep;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    public int getNextStep() {
-        return nextStep;
-    }
+  public int getNextStep() {
+    return nextStep;
+  }
 
-    public void setNextStep(int nextStep) {
-        this.nextStep = nextStep;
-    }
+  public void setNextStep(int nextStep) {
+    this.nextStep = nextStep;
+  }
 
 }
