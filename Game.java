0cd2c14e64c344197e035c3064a6f16318df7737
DerKrull <ls2016@web.de>
2020-12-17 15:38:58 +0100

    Fixed Tab characters and Identation level 2

--- Game.java
+++ Game.java
@@ -3,45 +3,45 @@ import java.util.Scanner;
 
 public class Game {
   public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);	    
+    Scanner in = new Scanner(System.in);    
     //TODO add all Questions
     LiveStep[] steps = new LiveStep[10];
     steps[0] = new LiveStep(1, "Das ist die erste Frage", new LiveChoice[]{
       new LiveChoice("1 - Antwort 1", 2),
       new LiveChoice("2 - Antwort 2", 1)
-    });
+      });
     steps[1] = new LiveStep(1, "Ergebnis 1", new LiveChoice[]{
       new LiveChoice("1 - weiter", 3)
-    });
+      });
     steps[2] = new LiveStep(1, "Ergebnis 2", new LiveChoice[]{
       new LiveChoice("1 - weiter", 3)
-    });
+      });
     steps[3] = new LiveStep(1, "Nächste Frage", new LiveChoice[]{
       new LiveChoice("1 - Antwort 1", 4),
       new LiveChoice("2 - Antwort 2", 5)
-    });
-	
+      });
+
     int id = 0;
     LiveStep currentStep = steps[id];
     boolean gameOver = true;
-    clearScreen();	
+    clearScreen();
 
-    while(gameOver){
+    while (gameOver) {
       System.out.println(currentStep.getDescription());
 	  LiveChoice[] choices = currentStep.getChoices();
 
-	  for(int i = 0; i < choices.length; i++){
+      for (int i = 0; i < choices.length; i++) {
         System.out.println(choices[i].getDescription());
       }
 
-        int answer = in.nextInt();
-        id = choices[answer - 1].getNextStep();
-        currentStep = steps[id];
-        if(id == 9){
-          System.out.println("Game Over");
-          gameOver = false;
-	    }
-          clearScreen();	   
+      int answer = in.nextInt();
+      id = choices[answer - 1].getNextStep();
+      currentStep = steps[id];
+      if (id == 9){
+        System.out.println("Game Over");
+        gameOver = false;
+	  }
+      clearScreen(); 
     }
   }
 
