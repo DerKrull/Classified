c122f786e0a4a455b50f2338812c6fbdad97d26f
DerKrull <ls2016@web.de>
2020-12-17 15:57:12 +0100

    Fixed Identation level

--- LiveStep.java
+++ LiveStep.java
@@ -1,39 +1,36 @@
 public class LiveStep {
-    private int id;
-    private String description;
+  private int id;
+  private String description;
+  private LiveChoice[] choices;
 
-    private LiveChoice[] choices;
+  public LiveStep(int id, String description, LiveChoice[] choices) {
+    this.id = id;
+    this.description = description;
+    this.choices = choices;   
+  }
 
-    public LiveStep(int id, String description, LiveChoice[] choices) {
-        this.id = id;
-        this.description = description;
-        this.choices = choices;
-       
-    }
+  public int getId() {
+    return id;
+  }
 
-    public int getId() {
-        return id;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public LiveChoice[] getChoices() {
+    return choices;
+  }
 
-    public LiveChoice[] getChoices() {
-        return choices;
-    }
-
-    public void setChoices(LiveChoice[] choices) {
-        this.choices = choices;
-    }
-
+  public void setChoices(LiveChoice[] choices) {
+    this.choices = choices;
+  }
 
 }
