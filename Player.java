72f6df92ec394ff6ce77e0521030e6fec948b57b
DerKrull <ls2016@web.de>
2021-01-19 17:14:00 +0100

    Add class Player

---
+++ Player.java
@@ -0,0 +1,34 @@
+public class Player {
+    private String name;
+    private char gender;
+    private int kontostand = 0;
+
+    public Player(String name, char gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getKontostand() {
+        return kontostand;
+    }
+
+    public void setKontostand(int kontostand) {
+        this.kontostand = kontostand;
+    }
+}
