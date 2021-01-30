0142ebbb93b9d101f8a7666e19700bd29f764e75
DerKrull <ls2016@web.de>
2021-01-30 01:30:17 +0100

    Add new attributes addMoney and removeMoney

--- LiveChoice.java
+++ LiveChoice.java
@@ -1,6 +1,8 @@
 public class LiveChoice {
   private String description;
   private int nextStep;
+  private int addMoney;
+  private int removeMoney;
 
   public LiveChoice(String description, int nextStep) {
     this.description = description;
@@ -21,6 +23,22 @@ public class LiveChoice {
 
   public void setNextStep(int nextStep) {
     this.nextStep = nextStep;
+  }
+
+  public int getAddMoney() {
+    return addMoney;
+  }
+
+  public void setAddMoney(int addMoney) {
+    this.addMoney = addMoney;
+  }
+
+  public int getRemoveMoney() {
+    return removeMoney;
+  }
+
+  public void setRemoveMoney(int removeMoney) {
+    this.removeMoney = removeMoney;
   }
 
 }
